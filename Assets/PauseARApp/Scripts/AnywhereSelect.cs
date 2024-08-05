8f74d174dc621d8211889203c2d9c2c469ba5ac2
Zayne Kadry <2092807k@student.gla.ac.uk>
2024-08-05 10:45:43 +0100

    walk anywhere timer update

--- Assets/PauseARApp/Scripts/AnywhereSelect.cs
+++ Assets/PauseARApp/Scripts/AnywhereSelect.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class AnywhereSelect : MonoBehaviour
 {
     public Button startButton;
     public Button backButton;
+    public Button limitlessButton;
     public Slider timerSlider;
     private int selectedTime = 10;
+    public TMP_Text selectedTimeText;
 
     // Start is called before the first frame update
     void Start()
@@ -18,25 +21,44 @@ public class AnywhereSelect : MonoBehaviour
         // Assign the button click listeners
         startButton.onClick.AddListener(OnStartButtonClick);
         backButton.onClick.AddListener(OnBackButtonClick);
+        limitlessButton.onClick.AddListener(OnLimitlessButtonClick);
 
         // initialise slider defaults
-        timerSlider.minValue = 10;
+        timerSlider.minValue = 1;
         timerSlider.maxValue = 30;
-        timerSlider.value = selectedTime;
+        timerSlider.value = 10;
+        timerSlider.wholeNumbers = true; // increment in whole numbers (minutes)
+
+        // update displayed time
+        UpdateSelectedTimeText(timerSlider.value);
+
+        // listener for slider to update selected time text
+        timerSlider.onValueChanged.AddListener(delegate { UpdateSelectedTimeText(timerSlider.value); });
         
     }
 
     void OnStartButtonClick()
     {
-        selectedTime = (int)timerSlider.value; // get selected duration
+        int selectedTime = (int)timerSlider.value; // get selected duration
         PlayerPrefs.SetInt("SelectedTime", selectedTime); // store selected time
         SceneManager.LoadScene("WalkAnywhere"); // load the Walk mode scene
+    }
+
+    void OnLimitlessButtonClick()
+    {
+        PlayerPrefs.SetInt("SelectedTime", -1); // store -1 to indicate limitless mode
+        SceneManager.LoadScene("WalkAnywhere");
     }
 
     void OnBackButtonClick()
     {
         // Load the Experience Select scene
         SceneManager.LoadScene("ChooseYourExperience");
+    }
+
+    void UpdateSelectedTimeText(float value)
+    {
+        selectedTimeText.text = value.ToString("0") + " minutes.";
     }
 
 }
--- Assets/PauseARApp/Scripts/WalkAnywhereTimer.cs
+++ Assets/PauseARApp/Scripts/WalkAnywhereTimer.cs
@@ -8,32 +8,59 @@ using TMPro;
 
 public class WalkAnywhereTimer : MonoBehaviour
 {
-    public float startTime;
     public TMP_Text timerText;
     public GameObject endPopup;
     public Button exitButton;
     public Button menuButton;
 
+    private bool isLimitless = false;
+    private float startTime;
+
     // Start is called before the first frame update
     void Start()
     {
         int selectedTime = PlayerPrefs.GetInt("SelectedTime", 10); // get selected time from PlayerPrefs. default to 10 if not found
-        startTime = Time.time + selectedTime * 60f; // convert minutes to secs
-        UpdateTimer();
+
+        if (selectedTime == -1) // check for limitless mode
+        {
+            isLimitless = true;
+            startTime = Time.time; // set time to current time
+        }
+        else
+        {
+            startTime = Time.time + selectedTime * 60f; // convert minutes to secs
+            UpdateTimer();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.time > startTime)
+        if (isLimitless)
         {
-            EndTimer(); // call method to handlle end of timer
+            UpdateLimitlessTimer();
+        }
+        else{
+            UpdateTimer();
+            
+            if (Time.time > startTime)
+            {
+                EndTimer(); // call method to handlle end of timer
+            }
         }
     }
 
     void UpdateTimer()
     {
         float currentTime = Mathf.FloorToInt(startTime - Time.time);
+        int minutes = Mathf.FloorToInt(currentTime / 60);
+        int seconds = Mathf.FloorToInt(currentTime % 60);
+        timerText.text = string.Format("{0:D2}:{1:D2}", minutes, seconds);
+    }
+
+    void UpdateLimitlessTimer()
+    {
+        float currentTime = Mathf.FloorToInt(Time.time - startTime);
         int minutes = Mathf.FloorToInt(currentTime / 60);
         int seconds = Mathf.FloorToInt(currentTime % 60);
         timerText.text = string.Format("{0:D2}:{1:D2}", minutes, seconds);
